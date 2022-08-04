512ccf26633965f436047ede64d93fa3ecd04de6
donggaa <kishalayc.donggaa@gmail.com>
2022-08-04 01:30:00 -0500

    Initial Commit

---
+++ src/test/java/com/costco/costcoautomation/test/SearchAutomationTest.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package com.costco.costcoautomation.test;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
+import java.time.Duration;
+import org.apache.commons.io.FileUtils;
+
+public class SearchAutomationTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:\\work\\softwares\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void searchTest() throws Exception {
+    driver.get("https://www.costco.com/appliances.html");
+    driver.findElement(By.xpath("//img[contains(@src,'https://mobilecontent.costco.com/live/resource/img/static-us-tiles/refrigerators.jpg')]")).click();
+    driver.get("https://www.costco.com/refrigerators.html");
+    driver.findElement(By.xpath("//div[@id='accordion-filter_collapse-1']/div/div[5]/a/label")).click();
+    driver.get("https://www.costco.com/french-door-refrigerators.html");
+    driver.findElement(By.linkText("Price")).click();
+    driver.findElement(By.xpath("//div[@id='accordion-filter_collapse-4']/div/span/div/label")).click();
+    driver.findElement(By.linkText("Brand")).click();
+    driver.findElement(By.xpath("//div[@id='accordion-filter_collapse-5']/div/span[4]/div/label")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
