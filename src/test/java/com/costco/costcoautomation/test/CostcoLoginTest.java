e5091697f47d58ddf53145350e7c5f7f75b7e897
donggaa <kishalayc.donggaa@gmail.com>
2022-08-05 22:11:40 -0500

    added logs

--- src/test/java/com/costco/costcoautomation/test/CostcoLoginTest.java
+++ src/test/java/com/costco/costcoautomation/test/CostcoLoginTest.java
@@ -55,9 +55,9 @@ public class CostcoLoginTest {
     System.out.println("isDisplayed : " + e.isDisplayed());
     System.out.println("isEnabled : " + e.isEnabled());
     
-    WebDriverWait t = new WebDriverWait(driver, Duration.ofSeconds(30)); 
-    t.until(ExpectedConditions.visibilityOf(e));  
-    t.until(ExpectedConditions.elementToBeClickable(e));
+    //WebDriverWait t = new WebDriverWait(driver, Duration.ofSeconds(30)); 
+    //t.until(ExpectedConditions.visibilityOf(e));  
+    //t.until(ExpectedConditions.elementToBeClickable(e));
     
     e.click();
     driver.get("https://signin.costco.com/e0714dd4-784d-46d6-a278-3e29553483eb/B2C_1A_SSO_WCS_signup_signin_500/oauth2/v2.0/authorize?ClientName=USBC&ui_locales=en-US&scope=openid&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.costco.com%2FOAuthLogonCmd&state=DM_PersistentCookieCreated%3Dtrue%26URL%3D%5BSSO%5D%2F%5BSSO%5D&nonce=NewdBHoWnpqSybT2&client_id=4900eb1f-0c10-4bd9-99c3-c59e6c1ecebf&TC=0&response_mode=form_post");
